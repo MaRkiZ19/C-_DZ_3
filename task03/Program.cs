a6f186103cce93742d94d1e91ff9f3a64920c95c
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-04 21:42:35 +0300

    task03

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,3 +1,13 @@
 ﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+int Read()
+{
+    System.Console.Write("Number N: ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int n = Read();
+int stepen = 3;
+for (int i=1; i<=n; i++)
+     Console.WriteLine("i^3 = " + Math.Pow(i, stepen));
 
